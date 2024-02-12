43707afe8466618d647081e8a7591bb00ac32f12
Evan Geer <egeer@kitconnect.com>
2024-02-12 13:27:32 -0700

    answers

---
+++ StackOverflowAnswers.Revit/StackOverflowAnswers.App/DataGridConditionalHeaderFormat.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace StackOverflowAnswers.Wpf
+{
+    /// <summary>
+    /// Interaction logic for DataGridConditionalHeaderFormat.xaml
+    /// </summary>
+    public partial class DataGridConditionalHeaderFormat : Window
+    {
+        public class dataMcData
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+        }
+        public DataGridConditionalHeaderFormat()
+        {
+            DataContext = new
+            {
+                Data = new List<dataMcData>
+                {
+                    new dataMcData {Name= "Test 1" , Number = 1},
+                    new dataMcData {Name= "Test 2" , Number = 2},
+                    new dataMcData {Name= "Test 3" , Number = 3},
+                }
+            };
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ StackOverflowAnswers.Revit/StackOverflowAnswers.App/GlobalHotKey.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StackOverflowAnswers.Wpf
+{
+public class CtrlLeftClickCommand : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter) => true;
+    public void Execute(object parameter)
+    {
+        DoJob();
+    }
+    private void DoJob() => MessageBox.Show("Job Done!");
+}
+
+public class ViewModel
+{
+    public ICommand CtrlLeftClickCommand { get; set; } = new CtrlLeftClickCommand();
+}
+/// <summary>
+/// Interaction logic for GlobalHotKey.xaml
+/// </summary>
+public partial class GlobalHotKey : Window
+{
+    public GlobalHotKey()
+    {
+        DataContext = new ViewModel();
+        InitializeComponent();
+    }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+            {
+                //e.Source = this;
+                //e.Handled = true;
+                //this.RaiseEvent(new MouseEventArgs(Mouse.PrimaryDevice, DateTime.Now.Millisecond)) ;
+                return;
+            }
+            MessageBox.Show("Click");
+        }
+    }
+}
